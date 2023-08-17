5da63880f568137b97454bed14d89430b215bc78
Luann2003 <lvictorluciano@outlook.com>
2023-08-17 19:41:04 -0300

    security

--- src/main/java/com/devsuperior/demo/entities/Role.java
+++ src/main/java/com/devsuperior/demo/entities/Role.java
@@ -1,32 +1,25 @@
 package com.devsuperior.demo.entities;
 
-import java.util.HashSet;
 import java.util.Objects;
-import java.util.Set;
+
+import org.springframework.security.core.GrantedAuthority;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.JoinTable;
-import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 
+@SuppressWarnings("serial")
 @Entity
 @Table(name = "tb_role")
-public class Role {
+public class Role implements GrantedAuthority{
 	
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String authority;
 	
-	@ManyToMany
-	@JoinTable(name = "tb_user_role",
-	        joinColumns = @JoinColumn(name = "user_id"),
-	        inverseJoinColumns = @JoinColumn(name = "role_id"))
-	private Set<Role> roles = new HashSet<>();
 	
 	public Role() {
 	}
@@ -43,7 +36,8 @@ public class Role {
 	public void setId(Long id) {
 		this.id = id;
 	}
-
+	
+	@Override
 	public String getAuthority() {
 		return authority;
 	}
@@ -57,18 +51,16 @@ public class Role {
 		return Objects.hash(authority);
 	}
 
-	public void addRole(Role role) {
-		roles.add(role);
-	}
 	
-	public boolean HasRole(String roleName) {
-		for(Role role: roles) {
-			if(role.getAuthority().equals(roleName)) {
-				return true;
-			}
-		}
-			return false;
-	}
+//	
+//	public boolean HasRole(String roleName) {
+//		for(Role role: roles) {
+//			if(role.getAuthority().equals(roleName)) {
+//				return true;
+//			}
+//		}
+//			return false;
+//	}
 	
 	@Override
 	public boolean equals(Object obj) {
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,5 @@
-INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '123456');
-INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '123456');
+INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '$2a$10$4CRVa7fp3WUIVF2zSA3OYe/HO044Vx3.UnltkJxdSzuKm/rQGX2gC');
+INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '$2a$10$4CRVa7fp3WUIVF2zSA3OYe/HO044Vx3.UnltkJxdSzuKm/rQGX2gC');
 
 INSERT INTO tb_role (authority) VALUES ('ROLE_OPERATOR');
 INSERT INTO tb_role (authority) VALUES ('ROLE_ADMIN');
